3980280e0043a92246c1cbb52ec46f9db49b670f
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-10-24 09:06:53 +0530

    Fixed: server response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,39 +2,39 @@ import { prisma } from "../server.js";
 
 // User signup: create a new user
 export const signup = async (req, res) => {
-    try {
-        console.log(req.body);
-        const { name, email, password } = req.body;
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password,
-            },
-        });
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-        console.error(error);
-    }
+  try {
+    console.log(req.body);
+    const { name, email, password } = req.body;
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    res.status(201).json({ name: name, email: email });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.error(error);
+  }
 };
 
 // User login: find user and authenticate
 export const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await prisma.user.findFirst({
-            where: {
-                OR: [{ email }, { password }],
-            },
-        });
-        if (!user) {
-            res.status(401).json({ error: "Invalid credentials" });
-            return;
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-        console.error(error);
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { password }],
+      },
+    });
+    if (!user) {
+      res.status(401).json({ error: "Invalid credentials" });
+      return;
     }
+    res.status(200).json({ name: user.name, email: user.email });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    console.error(error);
+  }
 };
